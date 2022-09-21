ffe129354ee6edd1b1aed7d456fcd130851047de
Dir-A <g2576612205@gmail.com>
2022-09-22 01:24:25 +0800

    NULL

--- CmvsFileHook/CmvsFileLoader/CmvsFileLoader.cpp
+++ CmvsFileHook/CmvsFileLoader/CmvsFileLoader.cpp
@@ -4,14 +4,15 @@
 STARTUPINFOW si;
 PROCESS_INFORMATION pi;
 
-INT APIENTRY WinMain(_In_ HINSTANCE hInstance,_In_opt_ HINSTANCE hPrevInstance,_In_ LPSTR lpCmdLine,_In_ int nShowCmd)
+INT APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
 {
 	ZeroMemory(&si, sizeof(si));
 	ZeroMemory(&pi, sizeof(pi));
 	si.cb = sizeof(si);
 
 	LPCWSTR applicationName = L"cmvs32.exe";
-	if (DetourCreateProcessWithDllW(applicationName, NULL, NULL, NULL, FALSE, CREATE_SUSPENDED, NULL, NULL, &si, &pi, "CmvsFileHook.dll", NULL))
+	LPCSTR dllName = "CmvsFileHook.dll";
+	if (DetourCreateProcessWithDllW(applicationName, NULL, NULL, NULL, FALSE, CREATE_SUSPENDED, NULL, NULL, &si, &pi, dllName, NULL))
 	{
 		ResumeThread(pi.hThread);
 		CloseHandle(pi.hThread);
