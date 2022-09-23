c683bbaa91f8bced5ce10f6c2ae9a98ed7a98888
Dir-A <g2576612205@gmail.com>
2022-09-23 21:39:18 +0800

    Add cmvs3.80-1.10 support

--- CmvsFileHook/CmvsFileLoader/CmvsFileLoader.cpp
+++ CmvsFileHook/CmvsFileLoader/CmvsFileLoader.cpp
@@ -11,6 +11,13 @@ INT APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
 	si.cb = sizeof(si);
 
 	LPCWSTR applicationName = L"cmvs32.exe";
+	HANDLE hFile = CreateFileW(applicationName, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (hFile == INVALID_HANDLE_VALUE)
+	{
+		applicationName = L"cmvs.exe"; //For old version cmvs
+	}
+	CloseHandle(hFile);
+
 	LPCSTR dllName = "CmvsFileHook.dll";
 	if (DetourCreateProcessWithDllW(applicationName, NULL, NULL, NULL, FALSE, CREATE_SUSPENDED, NULL, NULL, &si, &pi, dllName, NULL))
 	{
